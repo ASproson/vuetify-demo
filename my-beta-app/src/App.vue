ea7aed9d416efaf042bc787390d70d6d1826e4c7
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-04-13 09:33:04 +0100

    Adds footer

--- my-beta-app/src/App.vue
+++ my-beta-app/src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <v-app>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-          <v-text-field
-            label="Password"
-            :type="showPassword ? 'text' : 'password'"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>Home</v-btn>
+      <v-btn text>Login</v-btn>
+    </v-app-bar>
+    <v-main>
+      <!-- Login Module -->
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+            <v-text-field
+              label="Password"
+              :type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-main>
+    <!-- Footer Module -->
+    <v-main>
+      <v-footer
+        color="primary lighten-1"
+        fixed
+        bottom
+        height="100"
+        width="100%"
+      >
+        <v-row justify="center" no-gutters>
+          <v-btn text v-for="link in links" :key="link">
+            {{ link }}
+          </v-btn>
+          <v-col
+            class="primary lighten-2 py-4 text-center white--text"
+            cols="12"
+          >
+            {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+    </v-main>
   </v-app>
 </template>
 
@@ -38,6 +69,7 @@ export default {
 
   data: () => ({
     showPassword: false,
+    links: ["Home", "Login"],
   }),
 };
 </script>
